0a6321ee9c0a799084a30dadea2e8190cad98864
Phoenix <adit.parida1@gmail.com>
2020-08-27 13:17:30 -0700

    Basic Setup
    Added rudimnentary logic to the base

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-
+export class RecipeListComponent implements OnInit 
+{
+  recipes: Recipe[] = [
+    new Recipe("Baingan ka Bharta","An Indian Recipe", "https://upload.wikimedia.org/wikipedia/commons/3/3a/Baingan_Ka_Bharta.JPG")
+  ];
+  PP = ["ASDASD", "ASDASDA"];
+  getRecipes()
+  {
+    return this.recipes;
+  }
   constructor() { }
-
+  getpp()
+  {
+    return this.PP;
+  }
   ngOnInit(): void {
   }
+  
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit 
+{
+  
+  ingredients: Ingredient[] =[
+    new Ingredient('Aubergine', 2),
+    new Ingredient('Potato', 3),
+    new Ingredient("Tomato", 1)
+  ] ;
 
   constructor() { }
-
+  
+  
   ngOnInit(): void {
   }
 
